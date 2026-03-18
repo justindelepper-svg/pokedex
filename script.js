dc5c1ed6927c23980f38247864c8951b12cf2056
Frikandelgod <justindelepper@gmail.com>
2026-03-18 17:58:55 +0100

    betere layout + header + lazy loading + goede volgorde + laptop responsive

--- script.js
+++ script.js
@@ -1,57 +1,95 @@
 document.addEventListener('DOMContentLoaded', function()
 {
     const responseContainer = document.getElementById('response-container');
+    const loadTrigger = document.getElementById('load-trigger');
+    let offset = 0;
+    const limit = 18;
+    let loading = false;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon`)
-    .then(response => response.json())
-    .then(data => makeCard(data))
-    .catch(e => requestError(e, 'pokemon'));
-
-    function makeCard(data)
+    const observer = new IntersectionObserver(entries =>
     {
-        console.log(data);
-
-        for (let i = 0; i < 10; i++)
+        // check of de maximale pokemon is bereikt (1350). als dat zo is, voer dan niet nog een batch uit
+        if(entries[0].isIntersecting && !loading)
         {
-            const pokemon = data.results[i];
-        
+            loading = true;
+            loadMorePokemon();
+        }
+    });
+
+    function callAPI(offset, limit)
+    {
+        return fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+        .then(response => response.json())
+        .then(data => makeCard(data))
+        .catch(e => requestError(e, 'pokemon'));
+    }
+    callAPI(offset, limit);
+
+    async function makeCard(data)
+    {
+        console.log("data: ", data);
+
+        const pokemons = data.results.slice(0, limit);
+        console.log("pokemons: ", pokemons);
+
+        const pokemonPromises = pokemons.map(pokemon =>
             fetch(pokemon.url)
             .then(response => response.json())
-            .then(pokeData =>
-            {
-                const name = pokeData.name.toUpperCase();
-                const sprite = pokeData.sprites.front_default;
-                // const sprite2 = pokeData.sprites.versions['generation-iv']['diamond-pearl'].front_default;
-                console.log(name, sprite);
+        );
 
-                const card = document.createElement('div');
-                card.classList.add('pokemon-card');
+        const pokeData = await Promise.all(pokemonPromises);
 
-                const imgElement = document.createElement('img');
-                imgElement.classList.add('pokemon-sprite');
-                imgElement.src = sprite;
+        pokeData.forEach((pokeData, index) =>
+        {
+            const id = pokeData.id;
+            const name = pokeData.name.toUpperCase();
+            const sprite = pokeData.sprites.front_default;
+            // const sprite2 = pokeData.sprites.versions['generation-iv']['diamond-pearl'].front_default;
+            console.log(name, sprite);
+
+            const card = document.createElement('div');
+            card.classList.add('pokemon-card');
+
+            const imgElement = document.createElement('img');
+            imgElement.classList.add('pokemon-sprite');
+            imgElement.src = sprite;
                 
-                const nameElement = document.createElement('h4');
-                nameElement.classList.add('pokemon-name');
-                nameElement.textContent = name;
+            const textElement = document.createElement('h5');
+            textElement.classList.add('pokemon-text');
+            textElement.textContent = id + ". " + name;
 
-                card.appendChild(imgElement);
-                card.appendChild(nameElement);
+            card.appendChild(imgElement);
+            card.appendChild(textElement);
 
-                responseContainer.appendChild(card);
+            responseContainer.insertBefore(card, loadTrigger);
 
-                // Maak de eerste kaart geselecteerd
-                if (responseContainer.children.length === 1)
-                {
-                    card.classList.add('card-selected');
-                }
-            });
-        }
+            // Maak de eerste kaart geselecteerd
+            if (index === 0)
+            {
+                card.classList.add('card-selected');
+            }
+        });
+
+        loading = false;
+
+        // pas als de gebruiker omlaag scrolt OF als de navigatie op 18+ komt, voer dan de volgende batch uit
+        nextBatch();
     }
 
     function requestError(error, type)
     {
         console.error(`Error fetching ${type}:`, error);
+    }
+
+    function nextBatch()
+    {
+        observer.observe(loadTrigger);
+    }
+
+    function loadMorePokemon()
+    {
+        offset += limit;
+        callAPI(offset, limit);
     }
 
     // -------------------------------------------------------------------------------------------------------------
@@ -60,15 +98,19 @@ document.addEventListener('DOMContentLoaded', function()
     let currentIndex = 0;
     let currentRotation = 10;
     const rotationValue = 30;
-    const rowLength = 5;
+    const rowLength = 6;
 
     document.addEventListener('keydown', function(event)
     {
+        if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key))
+        {
+            event.preventDefault();
+        }
+
         const cards = document.querySelectorAll('.pokemon-card');
 
         // Set the initial card as selected
         cards[currentIndex].classList.add('card-selected');
-
         
         if (event.key === 'ArrowUp')
         {
@@ -88,6 +130,12 @@ document.addEventListener('DOMContentLoaded', function()
 
             // Style the new current card
             cards[currentIndex].classList.add('card-selected');
+
+            cards[currentIndex].scrollIntoView
+            ({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
         }
         else if (event.key === 'ArrowRight')
         {
@@ -107,6 +155,12 @@ document.addEventListener('DOMContentLoaded', function()
 
             // Style the new current card
             cards[currentIndex].classList.add('card-selected');
+
+            cards[currentIndex].scrollIntoView
+            ({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
         }
         else if (event.key === 'ArrowDown')
         {
@@ -126,6 +180,12 @@ document.addEventListener('DOMContentLoaded', function()
 
             // Style the new current card
             cards[currentIndex].classList.add('card-selected');
+
+            cards[currentIndex].scrollIntoView
+            ({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
         } else if (event.key === 'ArrowLeft')
         {
             // Reset the current card
@@ -144,6 +204,12 @@ document.addEventListener('DOMContentLoaded', function()
 
             // Style the new current card
             cards[currentIndex].classList.add('card-selected');
+
+            cards[currentIndex].scrollIntoView
+            ({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
         }
     });
 });
